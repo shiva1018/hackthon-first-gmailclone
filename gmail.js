31a2318aaa6b0138c0428357e104e22a47e07e2a
shiva1018 <91366925+shiva1018@users.noreply.github.com>
2021-10-21 13:33:24 +0530

    hackthon first gmail clone

---
+++ gmail.js
@@ -0,0 +1,301 @@
+
+
+
+
+function composeme() {
+    document.body.innerHTML =`
+    <div class="msg-body">
+    <div class="msg1">
+    <h3 class="head">New Meassage
+    <i class="material-icons">minimize</i> 
+      <h3>
+<input class="msg" type="text" placeholder="from" value="abc@gmail.com">
+<input type ="text" placeholder="TO">
+
+<div class= "meassage" >
+<textarea class="subject"> subject.....</textarea>
+<div class="send">
+<button class="send">Send</button><i class="material-icons">attach_file</i> 
+<i class="material-icons">mood</i> 
+</div>
+</div>  
+</div>
+</div>`
+    console.log("compised.....")
+} 
+function sentmsg(){
+    document.body.innerHTML=`<div class="send">
+    <div class="sent msgslist">
+    <div class="email_msg">
+  <div class="email_contain">
+    <div class="email_rowoption">
+      <input type="checkbox">
+      <i class="material-icons">star_border</i>
+      <i class="material-icons">label</i>
+    </div>
+    <h3 class="title">TO:SSC help Desk</h3>
+    <div class="email_row_msgs">
+ <h4>
+   You get a subscribe
+   <span class="email_description">.on future coder</span>
+ </h4>
+
+    </div>
+    <p class="msg_time">3pm</p>
+    
+  </div>
+</div> 
+<div class="email_msg">
+  <div class="email_contain">
+    <div class="email_rowoption">
+      <input type="checkbox">
+      <i class="material-icons">star_border</i>
+      <i class="material-icons">label</i>
+    </div>
+    <h3 class="title">TO:NSP help desk</h3>
+    <div class="email_row_msgs">
+ <h4>
+   You get a subscribe
+   <span class="email_description">.on future coder</span>
+ </h4>
+
+    </div>
+    <p class="msg_time">3pm</p>
+    
+  </div>
+</div> 
+<div class="email_msg">
+  <div class="email_contain">
+    <div class="email_rowoption">
+      <input type="checkbox">
+      <i class="material-icons">star_border</i>
+      <i class="material-icons">label</i>
+    </div>
+    <h3 class="title"> TO: ABC@gmail.com</h3>
+    <div class="email_row_msgs">
+ <h4>
+   You get a subscribe
+   <span class="email_description">.on future coder</span>
+ </h4>
+
+    </div>
+    <p class="msg_time">3pm</p>
+    
+  </div>
+  <div class="email_msg">
+  <div class="email_contain">
+    <div class="email_rowoption">
+      <input type="checkbox">
+      <i class="material-icons">star_border</i>
+      <i class="material-icons">label</i>
+    </div>
+    <h3 class="title"> TO: ABC@gmail.com</h3>
+    <div class="email_row_msgs">
+ <h4>
+   You get a subscribe
+   <span class="email_description">.on future coder</span>
+ </h4>
+
+    </div>
+    <p class="msg_time">3pm</p>
+    
+  </div>
+  <div class="email_msg">
+  <div class="email_contain">
+    <div class="email_rowoption">
+      <input type="checkbox">
+      <i class="material-icons">star_border</i>
+      <i class="material-icons">label</i>
+    </div>
+    <h3 class="title"> TO: ABC@gmail.com</h3>
+    <div class="email_row_msgs">
+ <h4>
+   You get a subscribe
+   <span class="email_description">.on future coder</span>
+ </h4>
+
+    </div>
+    <p class="msg_time">3pm</p>
+    
+  </div>
+  <div class="email_msg">
+  <div class="email_contain">
+    <div class="email_rowoption">
+      <input type="checkbox">
+      <i class="material-icons">star_border</i>
+      <i class="material-icons">mode_edit</i>
+    </div>
+    <h3 class="title"> TO: ABC@gmail.com</h3>
+    <div class="email_row_msgs">
+ <h4>
+   You get a subscribe
+   <span class="email_description">.on future coder</span>
+ </h4>
+
+    </div>
+    <p class="msg_time">3pm</p>
+    
+  </div>
+  <div class="email_msg">
+  <div class="email_contain">
+    <div class="email_rowoption">
+      <input type="checkbox">
+      <i class="material-icons">star_border</i>
+      <i class="material-icons">mode_edit</i>
+    </div>
+    <h3 class="title"> TO: ABC@gmail.com</h3>
+    <div class="email_row_msgs">
+ <h4>
+   You get a subscribe
+   <span class="email_description">.on future coder</span>
+ </h4>
+
+    </div>
+    <p class="msg_time">7pm</p>
+    
+  </div>
+  <div class="email_msg">
+  <div class="email_contain">
+    <div class="email_rowoption">
+      <input type="checkbox">
+      <i class="material-icons">star_border</i>
+      <i class="material-icons">mode_edit</i>
+    </div>
+    <h3 class="title"> TO: ABC@gmail.com</h3>
+    <div class="email_row_msgs">
+ <h4>
+   You get a subscribe
+   <span class="email_description">.on future coder</span>
+ </h4>
+
+    </div>
+    <p class="msg_time">10pm</p>
+    
+  </div>
+</div> 
+</div>
+
+    </div>
+    `
+}
+function draftmsg() {
+    document.body.innerHTML=`<div class="draft">
+    </div>
+    <div class="email_msg">
+    <div class="email_contain">
+      <div class="email_rowoption">
+        <input type="checkbox">
+        <i class="material-icons">star_border</i>
+        <i class="material-icons">mode_edit</i>
+      </div>
+      <h3 class="title1"> Darft</h3>
+      <div class="email_row_msgs">
+   <h4>
+     You get a subscribe
+     <span class="email_description">.on future coder</span>
+   </h4>
+  
+      </div>
+      <p class="msg_time">3pm</p>
+      
+    </div>
+   
+
+    </div>
+    </div>
+    <div class="email_msg">
+    <div class="email_contain">
+      <div class="email_rowoption">
+        <input type="checkbox">
+        <i class="material-icons">star_border</i>
+        <i class="material-icons">mode_edit</i>
+      </div>
+      <h3 class="title1"> Darft</h3>
+      <div class="email_row_msgs">
+   <h4>
+     You get a subscribe
+     <span class="email_description">.on future coder</span>
+   </h4>
+  
+      </div>
+      <p class="msg_time">3pm</p>
+      
+    </div>
+    </div>
+    <div class="email_msg">
+    <div class="email_contain">
+      <div class="email_rowoption">
+        <input type="checkbox">
+        <i class="material-icons">star_border</i>
+        <i class="material-icons">mode_edit</i>
+      </div>
+      <h3 class="title1"> Darft</h3>
+      <div class="email_row_msgs">
+   <h4>
+     You get a subscribe
+     <span class="email_description">.on future coder</span>
+   </h4>
+  
+      </div>
+      <p class="msg_time">3pm</p>
+      
+    </div>
+    </div>
+    <div class="email_msg">
+    <div class="email_contain">
+      <div class="email_rowoption">
+        <input type="checkbox">
+        <i class="material-icons">star_border</i>
+        <i class="material-icons">mode_edit</i>
+      </div>
+      <h3 class="title1"> Darft</h3>
+      <div class="email_row_msgs">
+   <h4>
+     You get a subscribe
+     <span class="email_description">.on future coder</span>
+   </h4>
+  
+      </div>
+      <p class="msg_time">3pm</p>
+      
+    </div>
+    </div>
+    <div class="email_msg">
+    <div class="email_contain">
+      <div class="email_rowoption">
+        <input type="checkbox">
+        <i class="material-icons">star_border</i>
+        <i class="material-icons">mode_edit</i>
+      </div>
+      <h3 class="title1"> Darft</h3>
+      <div class="email_row_msgs">
+   <h4>
+     You get a subscribe
+     <span class="email_description">.on future coder</span>
+   </h4>
+  
+      </div>
+      <p class="msg_time">3pm</p>
+      
+    </div>
+    </div>
+    <div class="email_msg">
+    <div class="email_contain">
+      <div class="email_rowoption">
+        <input type="checkbox">
+        <i class="material-icons">star_border</i>
+        <i class="material-icons">mode_edit</i>
+      </div>
+      <h3 class="title1"> Darft</h3>
+      <div class="email_row_msgs">
+   <h4>
+     You get a subscribe
+     <span class="email_description">.on future coder</span>
+   </h4>
+  
+      </div>
+      <p class="msg_time">3pm</p>
+      
+    </div>
+    `
+}
